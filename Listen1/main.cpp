518daf83b5b7c5dbe04dd153084e53c04c6e2bb9
Dier Song <Dier.Song@videojet.com>
2016-12-15 12:54:55 +0800

    fix bug

--- Listen1/main.cpp
+++ Listen1/main.cpp
@@ -10,23 +10,34 @@ public:
     Listen1(QObject *parent=0);
     QTcpServer *server;
     void run();
+    void startListen();
 private:
 };
 Listen1::Listen1(QObject *parent)
     :QThread(parent)
 {
-    server = new QTcpServer(this);
+    server = new QTcpServer();
+}
+
+void Listen1::startListen()//启动服务器
+{
+    server->listen(QHostAddress::Any,6000);//listen函数
+  // server->listen(QHostAddress("192.168.1.110"),6000);
 }
 
 void Listen1::run()//启动服务器
 {
-    server->listen(QHostAddress::Any,6000);//listen函数
+  //  server->listen(QHostAddress::Any,6000);//listen函数
   // server->listen(QHostAddress("192.168.1.110"),6000);
 }
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     Listen1 lis;//创建对象
+    lis.startListen();
     lis.start();//启动线程
+    //QTcpServer *server = new QTcpServer();
+    //server->listen(QHostAddress::Any,6000);//listen函数
+
     return a.exec();
 }
